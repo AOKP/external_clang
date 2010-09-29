32a4baebfa2f579b3be3b31dce97658bd5414258
zonr <wade1227@gmail.com>
2010-09-29 22:03:27 +0800

    Add Android.mk to build libclangDriver and libclangFrontend. Todo: Include a small portion of files used in libslang.
    Change-Id: Ib8d1a1a7de8c6ef1a79f2551fb26c21dcdf5add2

---
+++ lib/Driver/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH:= $(call my-dir)
+
+# For the host only
+# =====================================================
+include $(CLEAR_VARS)
+include $(CLEAR_TBLGEN_VARS)
+
+TBLGEN_TABLES :=	\
+	DiagnosticCommonKinds.inc	\
+	DiagnosticDriverKinds.inc
+
+clang_driver_SRC_FILES :=	\
+	Arg.cpp	\
+	ArgList.cpp	\
+	Option.cpp	\
+	OptTable.cpp
+
+LOCAL_SRC_FILES := $(clang_driver_SRC_FILES)
+
+LOCAL_MODULE:= libclangDriver
+
+include $(CLANG_HOST_BUILD_MK)
+include $(CLANG_TBLGEN_RULES_MK)
+include $(BUILD_HOST_STATIC_LIBRARY)
