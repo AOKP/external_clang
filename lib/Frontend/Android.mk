66ad399df1fbf7f78561cf89b7d3f4f1a0764fd1
Zonr Chang <zonr@google.com>
2010-10-08 20:48:32 +0800

    Slim down the lib/Frontend/Android.mk.

--- lib/Frontend/Android.mk
+++ lib/Frontend/Android.mk
@@ -5,46 +5,14 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 include $(CLEAR_TBLGEN_VARS)
 
-TBLGEN_TABLES :=    \
-	Attrs.inc	\
-	AttrList.inc	\
-	CC1Options.inc	\
-	DeclNodes.inc	\
-	DiagnosticASTKinds.inc	\
+TBLGEN_TABLES :=	\
 	DiagnosticCommonKinds.inc	\
-	DiagnosticDriverKinds.inc	\
-	DiagnosticFrontendKinds.inc	\
-	DiagnosticLexKinds.inc	\
-	DiagnosticSemaKinds.inc	\
-	StmtNodes.inc
+	DiagnosticFrontendKinds.inc
 
 clang_frontend_SRC_FILES :=	\
-	ASTConsumers.cpp	\
-	ASTMerge.cpp	\
-	ASTUnit.cpp	\
-	Android.mk	\
-	BoostConAction.cpp	\
-	CMakeLists.txt	\
-	CacheTokens.cpp	\
-	CompilerInstance.cpp	\
-	CompilerInvocation.cpp	\
-	DeclXML.cpp	\
 	DependencyFile.cpp	\
-	DiagChecker.cpp	\
-	DocumentXML.cpp	\
-	FrontendAction.cpp	\
-	FrontendActions.cpp	\
-	FrontendOptions.cpp	\
-	InitHeaderSearch.cpp	\
-	InitPreprocessor.cpp	\
-	LangStandards.cpp	\
-	PrintPreprocessedOutput.cpp	\
-	StmtXML.cpp	\
 	TextDiagnosticBuffer.cpp	\
-	TextDiagnosticPrinter.cpp	\
-	TypeXML.cpp	\
-	VerifyDiagnosticsClient.cpp	\
-	Warnings.cpp
+	TextDiagnosticPrinter.cpp
 
 LOCAL_SRC_FILES := $(clang_frontend_SRC_FILES)
 
@@ -52,7 +20,5 @@ LOCAL_MODULE:= libclangFrontend
 LOCAL_MODULE_TAGS:= optional
 
 include $(CLANG_HOST_BUILD_MK)
-include $(CLANG_VERSION_INC_MK)
 include $(CLANG_TBLGEN_RULES_MK)
-include $(LLVM_GEN_INTRINSICS_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
