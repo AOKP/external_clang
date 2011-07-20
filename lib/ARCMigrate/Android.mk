3bfadf88f1607f4c9378ca982a8d17b339f0534e
Logan Chien <loganchien@google.com>
2011-07-20 21:30:02 +0800

    Apply changes to migrate to clang upstream r135574.
    Change-Id: Ia355350e9e4462a9f1a6031806fc954686b4b679

--- lib/ARCMigrate/Android.mk
+++ lib/ARCMigrate/Android.mk
@@ -5,30 +5,32 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 include $(CLEAR_TBLGEN_VARS)
 
-TBLGEN_TABLES :=    \
-        Attrs.inc \
-        AttrList.inc    \
-        DeclNodes.inc   \
-        DiagnosticCommonKinds.inc \
-        DiagnosticGroups.inc \
-        DiagnosticSemaKinds.inc \
-        StmtNodes.inc
+TBLGEN_TABLES := \
+  Attrs.inc \
+  AttrList.inc \
+  DeclNodes.inc \
+  DiagnosticCommonKinds.inc \
+  DiagnosticGroups.inc \
+  DiagnosticSemaKinds.inc \
+  StmtNodes.inc
 
 clang_arc_migrate_SRC_FILES := \
-        ARCMT.cpp \
-        ARCMTActions.cpp \
-        FileRemapper.cpp \
-        TransARCAssign.cpp \
-        TransAutoreleasePool.cpp \
-        TransBlockObjCVariable.cpp \
-        TransEmptyStatementsAndDealloc.cpp \
-        TransformActions.cpp \
-        Transforms.cpp \
-        TransProperties.cpp \
-        TransRetainReleaseDealloc.cpp \
-        TransUnbridgedCasts.cpp \
-        TransUnusedInitDelegate.cpp \
-        TransZeroOutPropsInDealloc.cpp \
+  ARCMT.cpp \
+  ARCMTActions.cpp \
+  FileRemapper.cpp \
+  PlistReporter.cpp \
+  TransAPIUses.cpp \
+  TransARCAssign.cpp \
+  TransAutoreleasePool.cpp \
+  TransBlockObjCVariable.cpp \
+  TransEmptyStatementsAndDealloc.cpp \
+  TransformActions.cpp \
+  Transforms.cpp \
+  TransProperties.cpp \
+  TransRetainReleaseDealloc.cpp \
+  TransUnbridgedCasts.cpp \
+  TransUnusedInitDelegate.cpp \
+  TransZeroOutPropsInDealloc.cpp
 
 LOCAL_SRC_FILES := $(clang_arc_migrate_SRC_FILES)
 
