a95cba91ab8df9ebc1507e649e808968a9a16821
Douglas Gregor <dgregor@apple.com>
2011-11-02 20:52:01 +0000

    Fix various minor issues find via unreachable code warnings, from Ahmed Charles!
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@143569 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Basic/Targets.cpp
+++ lib/Basic/Targets.cpp
@@ -3080,6 +3080,8 @@ public:
                                      TargetInfo::ConstraintInfo &Info) const {
     switch (*Name) {
     default:
+      return false;
+        
     case 'r': // CPU registers.
     case 'd': // Equivalent to "r" unless generating MIPS16 code.
     case 'y': // Equivalent to "r", backwards compatibility only.
